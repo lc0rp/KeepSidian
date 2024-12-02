9e84599af21f3ea09188b6d90d20b336b4e02a3a
Luke K (pr-0f3t) <2609441+lc0rp@users.noreply.github.com>
2024-12-01 19:10:18 -0500

    Updating version in config

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 // KEEPSIDIAN_SERVER_URL is the URL of the Keepsidian server
 // It should NOT end with a slash
-export const KEEPSIDIAN_SERVER_URL = 'http://localhost:8080';
-// export const KEEPSIDIAN_SERVER_URL = 'https://keepsidianserver-i55qr5tvea-uc.a.run.app'
+// export const KEEPSIDIAN_SERVER_URL = 'http://localhost:8080';
+export const KEEPSIDIAN_SERVER_URL = 'https://keepsidianserver-i55qr5tvea-uc.a.run.app'
